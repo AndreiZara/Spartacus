ab10ccd9c6348d997df7ef0d0a845113ac28dde9
victoxrz <victor.afteni24@gmail.com>
2024-02-23 23:09:24 +0200

    finished bundles, still offset/padding issues compared to the original index.html

--- Spartacus.Web/App_Start/BundleConfig.cs
+++ Spartacus.Web/App_Start/BundleConfig.cs
@@ -1,45 +1,68 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Optimization;
-using System.Web.Routing;
+﻿using System.Web.Optimization;
 
 namespace Spartacus.Web.App_Start
 {
-     public class BundleConfig
-     {
-          public static void RegisterBundles(BundleCollection bundles)
-          {
-               // Home style
-               bundles.Add(new StyleBundle("~/bundles/main/css").Include(
-                         "~/Content/style.css", new CssRewriteUrlTransform()));
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            // Home style
+            bundles.Add(new StyleBundle("~/bundles/main/css").Include(
+                      "~/Content/css/style.css", new CssRewriteUrlTransform()));
 
-               // Animate.css
-               bundles.Add(new StyleBundle("~/bundles/animate/css").Include(
-                         "~/Content/animate.min.css"));
+            // Animate.css
+            bundles.Add(new StyleBundle("~/bundles/animate/css").Include(
+                      "~/Content/animate.css", new CssRewriteUrlTransform()));
 
-               // Bootstrap style
-               bundles.Add(new StyleBundle("~/bundles/bootstrap/css").Include(
-                         "~/Content/bootstrap.min.css", new CssRewriteUrlTransform()));
+            // Bootstrap style
+            bundles.Add(new StyleBundle("~/bundles/bootstrap/css").Include(
+                      "~/Content/bootstrap.min.css", new CssRewriteUrlTransform()));
 
-               // Font Awesome icons style
-               bundles.Add(new StyleBundle("~/bundles/font-awesome/css").Include(
-                         "~/Content/font-awesome.min.css", new CssRewriteUrlTransform()));
+            // Font Awesome icons style
+            bundles.Add(new StyleBundle("~/bundles/font-awesome/css").Include(
+                      "~/Content/font-awesome.min.css", new CssRewriteUrlTransform()));
 
-               // Bootstrap
-               bundles.Add(new ScriptBundle("~/bundles/bootstrap/js").Include(
-                         "~/Scripts/bootstrap.min.js"));
-               // jQuery
-               bundles.Add(new ScriptBundle("~/bundles/jquery/js").Include(
-                         "~/Scripts/jquery-3.3.1.min.js"));
+            // IcoFont style
+            bundles.Add(new StyleBundle("~/bundles/icofont/css").Include(
+                "~/External/icofont/icofont.min.css", new CssRewriteUrlTransform()));
 
+            // Themify style
+            bundles.Add(new StyleBundle("~/bundles/themify/css").Include(
+                "~/External/themify/css/themify-icons.css", new CssRewriteUrlTransform()));
 
-               // jQuery Validation
-               bundles.Add(new ScriptBundle("~/bundles/validation/js").Include(
-                         "~/Scripts/jquery.validate.min.js"));
+            // Magnify style
+            bundles.Add(new StyleBundle("~/bundles/magnify/css").Include(
+                "~/Content/magnific-popup.css", new CssRewriteUrlTransform()));
 
+            // Slick Carousel style
+            bundles.Add(new StyleBundle("~/bundles/Slick/css").Include(
+                "~/Content/Slick/slick.css", new CssRewriteUrlTransform()));
 
-          }
-     }
+            // Slick Carousel theme style
+            bundles.Add(new StyleBundle("~/bundles/Slick/tcss").Include(
+                "~/Content/Slick/slick-theme.css", new CssRewriteUrlTransform()));
+
+            // Bootstrap
+            bundles.Add(new Bundle("~/bundles/bootstrap/js").Include(
+                      "~/Scripts/bootstrap.min.js"));
+            // jQuery
+            bundles.Add(new ScriptBundle("~/bundles/jquery/js").Include(
+                      "~/Scripts/jquery-3.7.1.js"));
+
+            // jQuery Validation
+            bundles.Add(new ScriptBundle("~/bundles/validation/js").Include(
+                      "~/Scripts/jquery.validate.min.js"));
+
+            // Slick Carousel script
+            bundles.Add(new ScriptBundle("~/bundles/Slick/js").Include(
+                "~/Scripts/Slick/slick.min.js"));
+
+            // Magnific Popup
+            bundles.Add(new ScriptBundle("~/bundles/magnific/dist/js").Include(
+                "~/Scripts/jquery.magnific-popup.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/js").Include(
+                "~/Scripts/js/script.js"));
+        }
+    }
 }
