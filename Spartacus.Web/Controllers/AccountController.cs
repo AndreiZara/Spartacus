ada0e3ddadd09c0612ec98eacfc7d9c769c3d6b7
victoxrz <victor.afteni24@gmail.com>
2024-03-05 10:22:24 +0200

    reordering

---
+++ Spartacus.Web/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace Spartacus.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Join()
+        {
+            return View();
+        }
+    }
+}
