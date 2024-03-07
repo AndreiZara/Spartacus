f9767bf172256e247a6ce4f9bc3411b238579921
victoxrz <victor.afteni24@gmail.com>
2024-03-07 20:36:58 +0200

    Add entities, models

---
+++ Spartacus.BusinessLogic/Core/UserApi.cs
@@ -0,0 +1,17 @@
+﻿using Spartacus.Domain.Entities.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spartacus.BusinessLogic.Core
+{
+    internal class UserApi
+    {
+        internal bool UserLoginAction(ULoginData data)
+        {
+            return true;
+        }
+    }
+}
--- Spartacus.Web/Controllers/AccountController.cs
+++ Spartacus.Web/Controllers/AccountController.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using Spartacus.Domain.Entities.User;
+using Spartacus.Web.Models;
+using System;
+using System.Web.Mvc;
 
 namespace Spartacus.Web.Controllers
 {
@@ -7,6 +10,33 @@ namespace Spartacus.Web.Controllers
         public ActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(UserLogin login)
+        {
+            if(ModelState.IsValid)
+            {
+                ULoginData data = new ULoginData
+                {
+                    Name = login.Username,
+                    Password = login.Password,
+                    Ip = Request.UserHostAddress,
+                    LoginDateTime = DateTime.Now
+                };
+            }
+
+            var userLogin = false; // RESULT FROM THE Business Logic
+
+            if(userLogin)
+            {
+                return RedirectToAction("Index", "Home");
+            } 
+            else
+            {
+                ModelState.AddModelError("DeBil", "DeBil");
+                return View();
+            }
         }
 
         public ActionResult Join()
