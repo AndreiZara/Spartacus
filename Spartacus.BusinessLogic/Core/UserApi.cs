2f62a45272b69d9e2978e6759f1190cb983162d5
mistic1265 <cebotavictor14@gmail.com>
2024-03-22 08:11:21 +0200

    Update BusinessLogic Domain Helper

--- Spartacus.BusinessLogic/Core/UserApi.cs
+++ Spartacus.BusinessLogic/Core/UserApi.cs
@@ -1,4 +1,3 @@
-﻿using Spartacus.Domain.Entities.User;
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- Spartacus.BusinessLogic/Interfaces/ISession.cs
+++ Spartacus.BusinessLogic/Interfaces/ISession.cs
@@ -1,9 +1,4 @@
 ﻿using Spartacus.Domain.Entities.User;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Spartacus.BusinessLogic.Interfaces
 {
