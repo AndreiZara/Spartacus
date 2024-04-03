f78591fc52891650d656131821eccf8b91e348d0
AndreiZara <zaraandrei222@gmail.com>
2024-04-03 15:19:36 +0300

    Add membership

---
+++ Spartacus.Web/models/Membership.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Spartacus.Web.Models
+{
+    public class Membership
+    {
+        public string Title { get; set; }
+        public int[4] Prices { get; set; }
+        public string Description { get; set; }
+    }
+}
