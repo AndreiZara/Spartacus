0b9c80538b59b22023757aac8ea1707b126060ca
victoxrz <victor.afteni24@gmail.com>
2024-04-29 00:25:14 +0300

    clean up namings for categories and admin api

--- Spartacus.Web/Controllers/BaseController.cs
+++ Spartacus.Web/Controllers/BaseController.cs
@@ -5,49 +5,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Spartacus.BusinessLogic;
+using Spartacus.BusinessLogic.Interfaces;
+using Spartacus.Web.Extension;
+using Spartacus.BusinessLogic.Interfaces;
+using Spartacus.BusinessLogic;
+using Spartacus.Web.Extension;
 
-namespace Spartacus.Web.Controllers
 {
     public class BaseController : Controller
     {
-        protected readonly ISession _session = new BussinesLogic().GetSessionBL();
 
-        protected void SessionStatus()
         {
-            var cookie = Request.Cookies["UserCookie"];
-            if (cookie != null)
-            {
-                var user = _session.GetUserByCookie(cookie.Value);
-                if (user != null)
+        {
                 {
+                    System.Web.HttpContext.Current.SetMySessionObject(profile);
                     System.Web.HttpContext.Current.Session["LoginStatus"] = "login";
                 }
-                else 
+                else
                 { 
                     Session.Abandon();
                     EatCookie();
                 }
             }
             else
-            {
+                {
                 System.Web.HttpContext.Current.Session["LoginStatus"] = "logout";
             }
         }
 
         protected void EatCookie()
         {
-            System.Web.HttpContext.Current.Session.Clear();
-            if (ControllerContext.HttpContext.Request.Cookies.AllKeys.Contains("UserCookie"))
-            {
-                var cookie = ControllerContext.HttpContext.Request.Cookies["UserCookie"];
-                if (cookie != null)
-                {
-                    cookie.Expires = DateTime.Now.AddDays(-1);
-                    ControllerContext.HttpContext.Response.Cookies.Add(cookie);
+                    System.Web.HttpContext.Current.Session.Clear();
+                    {
+                        if (cookie != null)
+                        {
+                            cookie.Expires = DateTime.Now.AddDays(-1);
+                            ControllerContext.HttpContext.Response.Cookies.Add(cookie);
+                        }
+                    }
+
+                    System.Web.HttpContext.Current.Session["LoginStatus"] = "logout";
                 }
             }
-
-            System.Web.HttpContext.Current.Session["LoginStatus"] = "logout";
+            else
+            {
+                System.Web.HttpContext.Current.Session["LoginStatus"] = "logout";
+            }
         }
     }
 }
