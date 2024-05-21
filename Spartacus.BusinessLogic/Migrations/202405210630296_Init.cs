a1ed6f7c4e4e3d0e99d6c6101c244e54b20b49c0
mistic1265 <cebotavictor14@gmail.com>
2024-05-21 13:07:36 +0300

    new function to add photo data

---
+++ Spartacus.BusinessLogic/Migrations/202405210630296_Init.cs
@@ -0,0 +1,18 @@
+﻿namespace Spartacus.BusinessLogic.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Init : DbMigration
+    {
+        public override void Up()
+        {
+            DropColumn("dbo.UTables", "ImageData");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.UTables", "ImageData", c => c.Binary());
+        }
+    }
+}
