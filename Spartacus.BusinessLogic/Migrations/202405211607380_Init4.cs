4add2a2ed1910756c0596fc111f0023e1e8b8639
mistic1265 <cebotavictor14@gmail.com>
2024-05-22 06:14:01 +0300

    finished image adding

---
+++ Spartacus.BusinessLogic/Migrations/202405211607380_Init4.cs
@@ -0,0 +1,18 @@
+﻿namespace Spartacus.BusinessLogic.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Init4 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.UTables", "FileName", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.UTables", "FileName");
+        }
+    }
+}
